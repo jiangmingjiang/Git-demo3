870549bd46ee2903502bcb003c593b2db3f5ef26
Layne <Layne@atguigu.com>
2023-05-04 13:08:28 +0800

    second-commit

--- src/main/java/com/jmj/GitTest.java
+++ src/main/java/com/jmj/GitTest.java
@@ -3,5 +3,6 @@ package com.jmj;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello I am first Git!");
+        System.out.println("Hello I am second Git!");
     }
 }
