6e9a3a438b2d7c37321cf313f08a3ebdd2ac0b0f
Layne <Layne@atguigu.com>
2023-05-04 13:09:05 +0800

    third-commit

--- src/main/java/com/jmj/GitTest.java
+++ src/main/java/com/jmj/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello I am first Git!");
         System.out.println("Hello I am second Git!");
+        System.out.println("Hello I am third Git!");
     }
 }
